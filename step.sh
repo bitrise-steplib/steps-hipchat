9ba47b60755f305f176aa939154758340f1e7401
Viktor Benei <viktor.benei@gmail.com>
2014-06-17 19:53:31 +0200

    removed not required ssl cert PEM download; step descriptor 'step.yml' added

--- step.sh
+++ step.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# SSL certificate fix so it can be executed in an isolated, non admin user
-curl http://curl.haxx.se/ca/cacert.pem > $HOME/cacert.pem
-export SSL_CERT_FILE=$HOME/cacert.pem
-
 bundle install
 ruby ./hipchat.rb
 exit $?
