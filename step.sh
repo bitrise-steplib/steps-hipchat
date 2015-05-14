d3d7bc95ff649c366f330b92c94e7114e20e0009
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-05-14 12:25:24 +0200

    step fixes mL - source back to bitrise-io

--- step.sh
+++ step.sh
@@ -9,69 +9,78 @@ echo "" > "${formatted_output_file_path}"
 
 # Input validation
 # - required
-if [  -z "$HIPCHAT_TOKEN" ]; then
+if [  -z "$HIPCHAT_TOKEN" ] ; then
   write_section_to_formatted_output '*Notice: `$HIPCHAT_TOKEN` is not provided!*'
   exit 1
 fi
 
-if [ -z "$HIPCHAT_ROOMID" ]; then
+if [ -z "$HIPCHAT_ROOMID" ] ; then
   write_section_to_formatted_output '*Notice: `$HIPCHAT_ROOMID` is not provided!*'
   exit 1
 fi
 
-if [ -z "$HIPCHAT_FROM_NAME" ]; then
+if [ -z "$HIPCHAT_FROM_NAME" ] ; then
   write_section_to_formatted_output '*Notice: `$HIPCHAT_FROM_NAME` is not provided!*'
   exit 1
 fi
 
 # - optional
-if [ -z "$HIPCHAT_MESSAGE" ]; then
+if [ -z "$HIPCHAT_MESSAGE" ] ; then
   write_section_to_formatted_output '*Notice: `$HIPCHAT_MESSAGE` is not provided!*'
 fi
 
-if [ -z "$HIPCHAT_MESSAGE_COLOR" ]; then
+if [ -z "$HIPCHAT_MESSAGE_COLOR" ] ; then
   write_section_to_formatted_output '*Notice: `$HIPCHAT_MESSAGE_COLOR` is not provided!*'
 fi
 
-if [ -z "$HIPCHAT_ERROR_FROM_NAME" ]; then
+if [ -z "$HIPCHAT_ERROR_FROM_NAME" ] ; then
   write_section_to_formatted_output '*Notice: `$HIPCHAT_ERROR_FROM_NAME` is not provided!*'
 fi
 
-if [ -z "$HIPCHAT_ERROR_MESSAGE_TEXT" ]; then
+if [ -z "$HIPCHAT_ERROR_MESSAGE_TEXT" ] ; then
   write_section_to_formatted_output '*Notice: `$HIPCHAT_ERROR_MESSAGE_TEXT` is not provided!*'
 fi
 
 # Build failed mode
-isBuildFailedMode="0" #success
-if [ -n "$STEPLIB_BUILD_STATUS"]; then
+isBuildFailedMode="0"
+if [ -n "$STEPLIB_BUILD_STATUS" ] ; then
   isBuildFailedMode="${STEPLIB_BUILD_STATUS}"
 fi
 
 # Curl params
 error_message="${HIPCHAT_MESSAGE}"
-if [ -n "$HIPCHAT_ERROR_MESSAGE_TEXT" ]; then
+if [ -n "$HIPCHAT_ERROR_MESSAGE_TEXT" ] ; then
   error_message="${HIPCHAT_ERROR_FROM_NAME}"
 fi
 
 error_from_name="${HIPCHAT_FROM_NAME}"
-if [ -n "$HIPCHAT_ERROR_FROM_NAME" ]; then
+if [ -n "$HIPCHAT_ERROR_FROM_NAME" ] ; then
   error_from_name="${HIPCHAT_ERROR_FROM_NAME}"
 fi
 
 message="${HIPCHAT_MESSAGE}"
-if [[ isBuildFailedMode == "1" ]]; then
+if [[ "${isBuildFailedMode}" == "1" ]] ; then
   message="${error_message}"
 fi
 
 from_name="${HIPCHAT_FROM_NAME}"
-if [[ isBuildFailedMode == "1" ]]; then
+if [[ "${isBuildFailedMode}" == "1" ]] ; then
   from_name="${error_from_name}"
 fi
 
 msg_color='yellow'
-if [ -n "$HIPCHAT_MESSAGE_COLOR" ]; then
+if [ -n "$HIPCHAT_MESSAGE_COLOR" ] ; then
   msg_color="${HIPCHAT_MESSAGE_COLOR}"
 fi
+
+echo "Configs:"
+echo " * BUILD_FAILED_MODE: $isBuildFailedMode"
+echo " * HIPCHAT_TOKEN: $HIPCHAT_TOKEN"
+echo " * HIPCHAT_ROOMID: $HIPCHAT_ROOMID"
+echo " * HIPCHAT_FROM_NAME: $from_name"
+echo " * HIPCHAT_MESSAGE_COLOR: $msg_color"
+echo " * HIPCHAT_MESSAGE: $message"
+echo
 
 
 urlencode() {
@@ -97,12 +106,12 @@ curl_response=`curl -d $CONFIG --data-urlencode "message=$message" "https://api.
 echo "curl_response: $curl_response"
 err_search=$(echo $curl_response | grep error)
 
-if [[ isBuildFailedMode == "0"]]; then
+if [[ "${isBuildFailedMode}" == "1" ]] ; then
   write_section_to_formatted_output "# Message send failed!"
   write_section_to_formatted_output "Error message:"
   write_section_to_formatted_output "    ${message}"
   exit 1
-elif [ "$err_search" == "" ]; then
+elif [ "$err_search" == "" ] ; then
   write_section_to_formatted_output "# Message successfully sent!"
   write_section_to_formatted_output "## From:"
   write_section_to_formatted_output "${HIPCHAT_FROM_NAME}"
