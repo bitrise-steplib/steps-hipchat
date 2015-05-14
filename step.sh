58321793ea03e3161232fae22414e849dd7cb8ba
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-05-14 14:01:58 +0200

    code style, depman

--- step.sh
+++ step.sh
@@ -19,8 +19,8 @@ if [ -z "$HIPCHAT_ROOMID" ] ; then
   exit 1
 fi
 
-if [ -z "$HIPCHAT_FROM_NAME" ] ; then
-  write_section_to_formatted_output '*Notice: `$HIPCHAT_FROM_NAME` is not provided!*'
+if [ -z "$HIPCHAT_FROMNAME" ] ; then
+  write_section_to_formatted_output '*Notice: `$HIPCHAT_FROMNAME` is not provided!*'
   exit 1
 fi
 
@@ -33,12 +33,12 @@ if [ -z "$HIPCHAT_MESSAGE_COLOR" ] ; then
   write_section_to_formatted_output '*Notice: `$HIPCHAT_MESSAGE_COLOR` is not provided!*'
 fi
 
-if [ -z "$HIPCHAT_ERROR_FROM_NAME" ] ; then
-  write_section_to_formatted_output '*Notice: `$HIPCHAT_ERROR_FROM_NAME` is not provided!*'
+if [ -z "$HIPCHAT_ERROR_FROMNAME" ] ; then
+  write_section_to_formatted_output '*Notice: `$HIPCHAT_ERROR_FROMNAME` is not provided!*'
 fi
 
-if [ -z "$HIPCHAT_ERROR_MESSAGE_TEXT" ] ; then
-  write_section_to_formatted_output '*Notice: `$HIPCHAT_ERROR_MESSAGE_TEXT` is not provided!*'
+if [ -z "$HIPCHAT_ERROR_MESSAGE" ] ; then
+  write_section_to_formatted_output '*Notice: `$HIPCHAT_ERROR_MESSAGE` is not provided!*'
 fi
 
 # Build failed mode
@@ -48,24 +48,18 @@ if [ -n "$STEPLIB_BUILD_STATUS" ] ; then
 fi
 
 # Curl params
-error_message="${HIPCHAT_MESSAGE}"
-if [ -n "$HIPCHAT_ERROR_MESSAGE_TEXT" ] ; then
-  error_message="${HIPCHAT_ERROR_FROM_NAME}"
-fi
-
-error_from_name="${HIPCHAT_FROM_NAME}"
-if [ -n "$HIPCHAT_ERROR_FROM_NAME" ] ; then
-  error_from_name="${HIPCHAT_ERROR_FROM_NAME}"
-fi
-
 message="${HIPCHAT_MESSAGE}"
 if [[ "${isBuildFailedMode}" == "1" ]] ; then
-  message="${error_message}"
+  if [ -n "$HIPCHAT_ERROR_FROMNAME" ] ; then
+    message="${HIPCHAT_ERROR_FROMNAME}"
+  fi
 fi
 
-from_name="${HIPCHAT_FROM_NAME}"
+from_name="${HIPCHAT_FROMNAME}"
 if [[ "${isBuildFailedMode}" == "1" ]] ; then
-  from_name="${error_from_name}"
+  if [ -n "$HIPCHAT_ERROR_FROMNAME" ] ; then
+    from_name="${HIPCHAT_ERROR_FROMNAME}"
+  fi
 fi
 
 msg_color='yellow'
@@ -77,7 +71,7 @@ echo "Configs:"
 echo " * BUILD_FAILED_MODE: $isBuildFailedMode"
 echo " * HIPCHAT_TOKEN: $HIPCHAT_TOKEN"
 echo " * HIPCHAT_ROOMID: $HIPCHAT_ROOMID"
-echo " * HIPCHAT_FROM_NAME: $from_name"
+echo " * HIPCHAT_FROMNAME: $from_name"
 echo " * HIPCHAT_MESSAGE_COLOR: $msg_color"
 echo " * HIPCHAT_MESSAGE: $message"
 echo
@@ -114,7 +108,7 @@ if [[ "${isBuildFailedMode}" == "1" ]] ; then
 elif [ "$err_search" == "" ] ; then
   write_section_to_formatted_output "# Message successfully sent!"
   write_section_to_formatted_output "## From:"
-  write_section_to_formatted_output "${HIPCHAT_FROM_NAME}"
+  write_section_to_formatted_output "${HIPCHAT_FROMNAME}"
   write_section_to_formatted_output "## To Room:"
   write_section_to_formatted_output "${HIPCHAT_ROOMID}"
   write_section_to_formatted_output "## Message:"
